acbb1b6265798428578ba06f3d39c29f8ead82b1
Lucas Menéndez <hi@lucasmenendez.me>
2024-12-03 08:35:04 +0100

    F/verify vote circuit (#3)
    * Verifies the proof coming from the user that proves a valid ballot (using recursion), and checks their inputs over the bn254 curve.
    * Verifies the ECDSA signature of Circom inputs.
    * Verifies that the public key using to sign the Circom inputs generates the same address that the user provides.
    * Verifies a Arbo merkle proof (the census proof of the user) over the bls12_377 curve.
    Total # of constrains: 3.122.417

---
+++ circuits/voteverifier/vote_verifier.go
@@ -0,0 +1,219 @@
+// voteverifier package contains the Gnark circuit definition that verifies a
+// vote package to be aggregated by the vote aggregator and included in a new
+// state transition. A vote package includes a ballot proof (generated from
+// a circom circuit with snarkjs), the public inputs of the ballot proof
+// circuit, the signature of the public inputs, and a census proof. The vote
+// package is valid if the ballot proof is valid if:
+//   - The public inputs of the ballot proof are valid (match with the hash
+//     provided).
+//   - The ballot proof is valid for the public inputs.
+//   - The public inputs of the verification circuit are valid (match with the
+//     hash provided).
+//   - The signature of the public inputs is valid for the public key of the
+//     voter.
+//   - The address derived from the user public key is part of the census, and
+//     verifies the census proof with the user weight provided.
+//
+// Public inputs:
+//   - InputsHash: The hash of all the inputs that could be public.
+//
+// Private inputs:
+//   - MaxCount: The maximum number of votes that can be included in the
+//     package.
+//   - ForceUniqueness: A flag that indicates if the votes in the package
+//     values should be unique.
+//   - MaxValue: The maximum value that a vote can have.
+//   - MinValue: The minimum value that a vote can have.
+//   - MaxTotalCost: The maximum total cost of the votes in the package.
+//   - MinTotalCost: The minimum total cost of the votes in the package.
+//   - CostExp: The exponent used to calculate the cost of a vote.
+//   - CostFromWeight: A flag that indicates if the cost of a vote is
+//     calculated from the weight of the user or from the value of the vote.
+//   - Address: The address of the voter.
+//   - UserWeight: The weight of the user that is voting.
+//   - EncryptionPubKey: The public key used to encrypt the votes in the
+//     package.
+//   - Nullifier: The nullifier of the votes in the package.
+//   - Commitment: The commitment of the votes in the package.
+//   - ProcessId: The process id of the votes in the package.
+//   - EncryptedBallot: The encrypted votes in the package.
+//   - CensusRoot: The root of the census tree.
+//   - CensusSiblings: The siblings of the address in the census tree.
+//   - Msg: The hash of the public inputs of the ballot proof but as scalar
+//     element of the Secp256k1 curve.
+//   - PublicKey: The public key of the voter.
+//   - Signature: The signature of the inputs hash.
+//   - CircomProof: The proof of the ballot proof.
+//   - CircomPublicInputsHash: The hash of the public inputs of the ballot proof.
+//   - CircomVerificationKey: The verification key of the ballot proof (fixed).
+//
+// Note: The inputs of the circom circuit should be provided as elements of
+// the bn254 scalar field, and the inputs of the gnark circuit should be
+// provided as elements of the current compiler field (BLS12377 expected).
+package voteverifier
+
+import (
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_emulated"
+	"github.com/consensys/gnark/std/hash/mimc"
+	"github.com/consensys/gnark/std/math/emulated"
+	"github.com/consensys/gnark/std/recursion/groth16"
+	"github.com/consensys/gnark/std/signature/ecdsa"
+	"github.com/vocdoni/gnark-crypto-primitives/emulated/bn254/twistededwards/mimc7"
+	address "github.com/vocdoni/gnark-crypto-primitives/emulated/ecdsa"
+	"github.com/vocdoni/gnark-crypto-primitives/tree/arbo"
+	"github.com/vocdoni/gnark-crypto-primitives/utils"
+)
+
+type VerifyVoteCircuit struct {
+	// Single public input that is the hash of all the public inputs
+	InputsHash frontend.Variable `gnark:",public"`
+	// The following variables are priv-public inputs, so should be hashed
+	// and compared with the InputsHash or CircomPublicInputsHash. All the
+	// variables should be hashed in the same order as they are defined here.
+	MaxCount         emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	ForceUniqueness  emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	MaxValue         emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	MinValue         emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	MaxTotalCost     emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	MinTotalCost     emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	CostExp          emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	CostFromWeight   emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	Address          emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	UserWeight       emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	EncryptionPubKey [2]emulated.Element[sw_bn254.ScalarField]       // Part of CircomPublicInputsHash & InputsHash
+	Nullifier        emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	Commitment       emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	ProcessId        emulated.Element[sw_bn254.ScalarField]          // Part of CircomPublicInputsHash & InputsHash
+	EncryptedBallot  [8][2][2]emulated.Element[sw_bn254.ScalarField] // Part of CircomPublicInputsHash & InputsHash
+	CensusRoot       frontend.Variable                               // Part of InputsHash
+	CensusSiblings   [160]frontend.Variable
+	// The following variables are private inputs and they are used to verify
+	// the user identity ownership
+	Msg       emulated.Element[emulated.Secp256k1Fr]
+	PublicKey ecdsa.PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]
+	Signature ecdsa.Signature[emulated.Secp256k1Fr]
+	// The following variables are private inputs and they are used to verify
+	// the ballot proof
+	CircomProof            groth16.Proof[sw_bn254.G1Affine, sw_bn254.G2Affine]
+	CircomPublicInputsHash groth16.Witness[sw_bn254.ScalarField]
+	CircomVerificationKey  groth16.VerifyingKey[sw_bn254.G1Affine, sw_bn254.G2Affine, sw_bn254.GTEl] `gnark:"-"`
+}
+
+// nativeMiMCHashFn is a hash function that hashes the data provided using the
+// mimc hash function and the current compiler field. It is used to hash the
+// leaves of the census tree during the proof verification.
+func nativeMiMCHashFn(api frontend.API, data ...frontend.Variable) (frontend.Variable, error) {
+	h, err := mimc.NewMiMC(api)
+	if err != nil {
+		return 0, err
+	}
+	h.Write(data...)
+	return h.Sum(), nil
+}
+
+// circomInputs returns the circom public-private inputs that are used to hash
+// them and compare them with the unique public input of the circom circuit. It
+// asserts that the length of the flat encrypted ballot is correct and returns
+// the circom public-private inputs.
+func (c *VerifyVoteCircuit) circomInputs(api frontend.API) []emulated.Element[sw_bn254.ScalarField] {
+	// group the circom public-private inputs to hash them
+	circomPubPrivInputs := []emulated.Element[sw_bn254.ScalarField]{
+		c.MaxCount, c.ForceUniqueness, c.MaxValue, c.MinValue, c.MaxTotalCost,
+		c.MinTotalCost, c.CostExp, c.CostFromWeight, c.Address, c.UserWeight,
+		c.ProcessId, c.EncryptionPubKey[0], c.EncryptionPubKey[1], c.Nullifier,
+		c.Commitment,
+	}
+	// flatten the encrypted ballot and append to the circom public-private
+	// inputs
+	var flatEncryptedBallot []emulated.Element[sw_bn254.ScalarField]
+	for i := 0; i < len(c.EncryptedBallot); i++ {
+		for j := 0; j < len(c.EncryptedBallot[i]); j++ {
+			flatEncryptedBallot = append(flatEncryptedBallot, c.EncryptedBallot[i][j][:]...)
+		}
+	}
+	// check the length of the resulting flat encrypted ballot and append it
+	api.AssertIsEqual(len(flatEncryptedBallot), len(c.EncryptedBallot)*len(c.EncryptedBallot[0])*len(c.EncryptedBallot[0][0]))
+	return append(circomPubPrivInputs, flatEncryptedBallot...)
+}
+
+// checkCircomProof checks the circom proof provided by the user. It hashes the
+// circom public-private inputs and compares them with the unique public input
+// of the circom circuit. It verifies the ballot proof using the verification
+// key provided. It returns the hash of the circom public-private inputs if the
+// verification succeeds and an error if it fails.
+func (c *VerifyVoteCircuit) checkCircomProof(api frontend.API) error {
+	// check that the circom witness only contains a single public input
+	// (the hash of all the public-private inputs)
+	api.AssertIsEqual(len(c.CircomPublicInputsHash.Public), 1)
+	// hash the circom public-private inputs and compare them with the unique
+	// public input of the circom circuit
+	circomInputs := c.circomInputs(api)
+	h, err := mimc7.NewMiMC(api)
+	if err != nil {
+		return err
+	}
+	h.Write(circomInputs...)
+	h.AssertSumIsEqual(c.CircomPublicInputsHash.Public[0])
+	// api.AssertIsEqual(circomInputsHash, pubCircomInputsHash)
+	// verify the ballot proof over the bn254 curve (used by circom)
+	verifier, err := groth16.NewVerifier[sw_bn254.ScalarField, sw_bn254.G1Affine, sw_bn254.G2Affine, sw_bn254.GTEl](api)
+	if err != nil {
+		return err
+	}
+	// return circomInputsHash, verifier.AssertProof(
+	return verifier.AssertProof(
+		c.CircomVerificationKey, c.CircomProof, c.CircomPublicInputsHash,
+		groth16.WithCompleteArithmetic())
+}
+
+func (c *VerifyVoteCircuit) Define(api frontend.API) error {
+	// check circom circuit stuff
+	if err := c.checkCircomProof(api); err != nil {
+		return err
+	}
+	// convert the circom public inputs hash from element of bn254 scalar field
+	// to the current compiler field as a variable
+	circomInputsHash, err := utils.PackScalarToVar(api, &c.CircomPublicInputsHash.Public[0])
+	if err != nil {
+		return err
+	}
+	// hash the circom inputs with the census root to be compared with the
+	// inputs hash provided by the user
+	inputsHash, err := nativeMiMCHashFn(api, []frontend.Variable{circomInputsHash, c.CensusRoot}...)
+	if err != nil {
+		return err
+	}
+	api.AssertIsEqual(c.InputsHash, inputsHash)
+	// check the signature of the circom inputs hash provided as Secp256k1
+	// emulated element
+	c.PublicKey.Verify(api, sw_emulated.GetCurveParams[emulated.Secp256k1Fp](), &c.Msg, &c.Signature)
+	// derive the address from the public key and check it matches the provided
+	// address
+	derivedAddr, censusAddress, err := address.DeriveAddress(api, c.PublicKey)
+	if err != nil {
+		return err
+	}
+	// convert the derived address from the scalar field of the bn254 curve to
+	// the current compiler field as a variable to compare it with the address
+	// derived from the public key and to be used in the census proof
+	address, err := utils.PackScalarToVar(api, &c.Address)
+	if err != nil {
+		return err
+	}
+	api.AssertIsEqual(address, derivedAddr)
+	// convert the user weight from the scalar field of the bn254 curve to the
+	// current compiler field as a variable to be used in the census proof
+	userWeight, err := utils.PackScalarToVar(api, &c.UserWeight)
+	if err != nil {
+		return err
+	}
+	// verify the census proof using the derived address and the user weight
+	// provided as leaf key-value, adn the root and siblings provided
+	if err := arbo.CheckInclusionProof(api, nativeMiMCHashFn, censusAddress,
+		userWeight, c.CensusRoot, c.CensusSiblings[:]); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ circuits/voteverifier/vote_verifier_test.go
@@ -0,0 +1,247 @@
+package voteverifier
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"math/big"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr/mimc"
+	"github.com/consensys/gnark/backend"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/math/emulated"
+	gecdsa "github.com/consensys/gnark/std/signature/ecdsa"
+	"github.com/consensys/gnark/test"
+	qt "github.com/frankban/quicktest"
+	"github.com/iden3/go-iden3-crypto/mimc7"
+	arbotree "github.com/vocdoni/arbo"
+	"github.com/vocdoni/circom2gnark/parser"
+	ptest "github.com/vocdoni/gnark-crypto-primitives/test"
+	ztest "github.com/vocdoni/vocdoni-z-sandbox/circuits/test"
+	"github.com/vocdoni/vocdoni-z-sandbox/encrypt"
+
+	"go.vocdoni.io/dvote/util"
+)
+
+const (
+	n_fields = 8
+	n_levels = 160
+)
+
+var (
+	ballotProofWasm = "../assets/circom/circuit/ballot_proof.wasm"
+	ballotProofPKey = "../assets/circom/circuit/ballot_proof_pkey.zkey"
+	ballotProofVKey = "../assets/circom/circuit/ballot_proof_vkey.json"
+
+	maxCount        = 5
+	forceUniqueness = 0
+	maxValue        = 16
+	minValue        = 0
+	costExp         = 2
+	costFromWeight  = 0
+	weight          = 10
+	fields          = ztest.GenerateBallotFields(maxCount, maxValue, minValue, forceUniqueness > 0)
+)
+
+func TestVerifyVoteCircuit(t *testing.T) {
+	c := qt.New(t)
+
+	// CLIENT SIDE CIRCOM CIRCUIT
+
+	// generate encryption key and user nonce k
+	encryptionKey := ztest.GenerateEncryptionTestKey()
+	encryptionKeyX, encryptionKeyY := encryptionKey.Point()
+	k, err := encrypt.RandK()
+	c.Assert(err, qt.IsNil)
+	// encrypt the ballots
+	cipherfields, plainCipherfields := ztest.CipherBallotFields(fields, n_fields, encryptionKey, k)
+	// generate voter account
+	privKey, pubKey, address, err := ztest.GenerateECDSAaccount()
+	c.Assert(err, qt.IsNil)
+	// generate the commitment
+	processID := util.RandomBytes(20)
+	secret := util.RandomBytes(16)
+	commitment, nullifier, err := ztest.MockedCommitmentAndNullifier(address.Bytes(), processID, secret)
+	c.Assert(err, qt.IsNil)
+	// group the circom inputs to hash
+	bigCircomInputs := []*big.Int{
+		big.NewInt(int64(maxCount)),
+		big.NewInt(int64(forceUniqueness)),
+		big.NewInt(int64(maxValue)),
+		big.NewInt(int64(minValue)),
+		big.NewInt(int64(math.Pow(float64(maxValue), float64(costExp))) * int64(maxCount)),
+		big.NewInt(int64(maxCount)),
+		big.NewInt(int64(costExp)),
+		big.NewInt(int64(costFromWeight)),
+		arbotree.BigToFF(arbotree.BN254BaseField, new(big.Int).SetBytes(address.Bytes())),
+		big.NewInt(int64(weight)),
+		arbotree.BigToFF(arbotree.BN254BaseField, new(big.Int).SetBytes(processID)),
+		encryptionKeyX,
+		encryptionKeyY,
+		nullifier,
+		commitment,
+	}
+	bigCircomInputs = append(bigCircomInputs, plainCipherfields...)
+	circomInputsHash, err := mimc7.Hash(bigCircomInputs, nil)
+	c.Assert(err, qt.IsNil)
+	// transform the inputs hash to the field of the curve used by the circuit,
+	// if it is not done, the circuit will transform it during witness
+	// calculation and the hash will be different
+	blsCircomInputsHash := arbotree.BigToFF(ecc.BLS12_377.ScalarField(), circomInputsHash)
+	// sign the inputs hash with the private key
+	rSign, sSign, err := ztest.SignECDSA(privKey, blsCircomInputsHash.Bytes())
+	// init circom inputs
+	circomInputs := map[string]any{
+		"fields":           ztest.BigIntArrayToStringArray(fields, n_fields),
+		"max_count":        fmt.Sprint(maxCount),
+		"force_uniqueness": fmt.Sprint(forceUniqueness),
+		"max_value":        fmt.Sprint(maxValue),
+		"min_value":        fmt.Sprint(minValue),
+		"max_total_cost":   fmt.Sprint(int(math.Pow(float64(maxValue), float64(costExp))) * maxCount), // (maxValue-1)^costExp * maxCount
+		"min_total_cost":   fmt.Sprint(maxCount),
+		"cost_exp":         fmt.Sprint(costExp),
+		"cost_from_weight": fmt.Sprint(costFromWeight),
+		"address":          arbotree.BigToFF(arbotree.BN254BaseField, new(big.Int).SetBytes(address.Bytes())).String(),
+		"weight":           fmt.Sprint(weight),
+		"process_id":       arbotree.BigToFF(arbotree.BN254BaseField, new(big.Int).SetBytes(processID)).String(),
+		"pk":               []string{encryptionKeyX.String(), encryptionKeyY.String()},
+		"k":                k.String(),
+		"cipherfields":     cipherfields,
+		"nullifier":        nullifier.String(),
+		"commitment":       commitment.String(),
+		"secret":           arbotree.BigToFF(arbotree.BN254BaseField, new(big.Int).SetBytes(secret)).String(),
+		"inputs_hash":      circomInputsHash.String(),
+	}
+	bCircomInputs, err := json.Marshal(circomInputs)
+	c.Assert(err, qt.IsNil)
+	// create the proof
+	circomProof, pubSignals, err := ztest.CompileAndGenerateProof(bCircomInputs, ballotProofWasm, ballotProofPKey)
+	c.Assert(err, qt.IsNil)
+	// transform cipherfields to gnark frontend.Variable
+	fBallots := [n_fields][2][2]emulated.Element[sw_bn254.ScalarField]{}
+	for i, c := range cipherfields {
+		fBallots[i] = [2][2]emulated.Element[sw_bn254.ScalarField]{
+			{
+				emulated.ValueOf[sw_bn254.ScalarField](c[0][0]),
+				emulated.ValueOf[sw_bn254.ScalarField](c[0][1]),
+			},
+			{
+				emulated.ValueOf[sw_bn254.ScalarField](c[1][0]),
+				emulated.ValueOf[sw_bn254.ScalarField](c[1][1]),
+			},
+		}
+	}
+	// generate a test census proof
+	testCensus, err := ptest.GenerateCensusProofForTest(ptest.CensusTestConfig{
+		Dir:           "../assets/census",
+		ValidSiblings: 10,
+		TotalSiblings: n_levels,
+		KeyLen:        20,
+		Hash:          arbotree.HashFunctionMiMC_BLS12_377,
+		BaseFiled:     arbotree.BLS12377BaseField,
+	}, address.Bytes(), new(big.Int).SetInt64(int64(weight)).Bytes())
+	c.Assert(err, qt.IsNil)
+	// transform siblings to gnark frontend.Variable
+	fSiblings := [n_levels]frontend.Variable{}
+	for i, s := range testCensus.Siblings {
+		fSiblings[i] = frontend.Variable(s)
+	}
+	// hash the inputs of gnark circuit (circom inputs hash + census root)
+	hFn := mimc.NewMiMC()
+	hFn.Write(blsCircomInputsHash.Bytes())
+	hFn.Write(testCensus.Root.Bytes())
+	inputsHash := new(big.Int).SetBytes(hFn.Sum(nil))
+	// parse input files
+	proof, placeHolders, _, err := parseCircomInputs(ballotProofVKey, circomProof, pubSignals)
+	c.Assert(err, qt.IsNil)
+	placeholder := VerifyVoteCircuit{
+		CircomProof:            placeHolders.Proof,
+		CircomPublicInputsHash: placeHolders.Witness,
+		CircomVerificationKey:  placeHolders.Vk,
+	}
+
+	// SERVER SIDE GNARK CIRCUIT
+
+	// init inputs
+	witness := VerifyVoteCircuit{
+		InputsHash: inputsHash,
+		// circom inputs
+		MaxCount:        emulated.ValueOf[sw_bn254.ScalarField](maxCount),
+		ForceUniqueness: emulated.ValueOf[sw_bn254.ScalarField](forceUniqueness),
+		MaxValue:        emulated.ValueOf[sw_bn254.ScalarField](maxValue),
+		MinValue:        emulated.ValueOf[sw_bn254.ScalarField](minValue),
+		MaxTotalCost:    emulated.ValueOf[sw_bn254.ScalarField](int(math.Pow(float64(maxValue), float64(costExp))) * maxCount),
+		MinTotalCost:    emulated.ValueOf[sw_bn254.ScalarField](maxCount),
+		CostExp:         emulated.ValueOf[sw_bn254.ScalarField](costExp),
+		CostFromWeight:  emulated.ValueOf[sw_bn254.ScalarField](costFromWeight),
+		Address:         emulated.ValueOf[sw_bn254.ScalarField](address.Big()),
+		UserWeight:      emulated.ValueOf[sw_bn254.ScalarField](weight),
+		EncryptionPubKey: [2]emulated.Element[sw_bn254.ScalarField]{
+			emulated.ValueOf[sw_bn254.ScalarField](encryptionKeyX),
+			emulated.ValueOf[sw_bn254.ScalarField](encryptionKeyY),
+		},
+		Nullifier:       emulated.ValueOf[sw_bn254.ScalarField](nullifier),
+		Commitment:      emulated.ValueOf[sw_bn254.ScalarField](commitment),
+		ProcessId:       emulated.ValueOf[sw_bn254.ScalarField](arbotree.BigToFF(arbotree.BN254BaseField, new(big.Int).SetBytes(processID))),
+		EncryptedBallot: fBallots,
+		// census proof
+		CensusRoot:     testCensus.Root,
+		CensusSiblings: fSiblings,
+		// signature
+		Msg: emulated.ValueOf[emulated.Secp256k1Fr](blsCircomInputsHash),
+		PublicKey: gecdsa.PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+			X: emulated.ValueOf[emulated.Secp256k1Fp](pubKey.X),
+			Y: emulated.ValueOf[emulated.Secp256k1Fp](pubKey.Y),
+		},
+		Signature: gecdsa.Signature[emulated.Secp256k1Fr]{
+			R: emulated.ValueOf[emulated.Secp256k1Fr](rSign),
+			S: emulated.ValueOf[emulated.Secp256k1Fr](sSign),
+		},
+		// circom proof
+		CircomProof:            proof.Proof,
+		CircomPublicInputsHash: proof.PublicInputs,
+	}
+	// generate proof
+	assert := test.NewAssert(t)
+	now := time.Now()
+	assert.SolvingSucceeded(&placeholder, &witness,
+		test.WithCurves(ecc.BLS12_377),
+		test.WithBackends(backend.GROTH16))
+	fmt.Println("proving tooks", time.Since(now))
+}
+
+func parseCircomInputs(vKeyFile string, rawProof, rawPubSignals string) (*parser.GnarkRecursionProof, *parser.GnarkRecursionPlaceholders, *big.Int, error) {
+	// load data from assets
+	vKeyData, err := os.ReadFile(vKeyFile)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	// transform to gnark format
+	gnarkProofData, err := parser.UnmarshalCircomProofJSON([]byte(rawProof))
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	gnarkPubSignalsData, err := parser.UnmarshalCircomPublicSignalsJSON([]byte(rawPubSignals))
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	gnarkVKeyData, err := parser.UnmarshalCircomVerificationKeyJSON(vKeyData)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	proof, placeHolders, err := parser.ConvertCircomToGnarkRecursion(gnarkProofData, gnarkVKeyData, gnarkPubSignalsData, true)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	// decode pub input to get the hash to sign
+	inputsHash, ok := new(big.Int).SetString(gnarkPubSignalsData[0], 10)
+	if !ok {
+		return nil, nil, nil, fmt.Errorf("failed to decode inputs hash")
+	}
+	return proof, placeHolders, inputsHash, nil
+}
