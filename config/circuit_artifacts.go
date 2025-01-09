9fbd910af11335bf554d3af37f1bbefcc9028baf
Lucas Menéndez <hi@lucasmenendez.me>
2025-01-09 16:56:02 +0100

    feature: download circuit artifacts (#13)
    * initial downloader
    * use dummy placeholders
    * circuits artifacts URLs
    * including artifacts hashes
    * new config package with default circuits variables
    * set the basedir by default to a ./.cache/circuits-artifacts and remove os reference from artifacts.go

---
+++ config/circuit_artifacts.go
@@ -0,0 +1,16 @@
+package config
+
+const (
+	// CircuitArtifacts constants for circuits/voteverifier package
+	VoteVerifierProvingKeyURL       = "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/voteverifier/voteverifier.pk"
+	VoteVerifierProvingKeyHash      = "4bcb2de78562f400a3f96e5adcdcc00d32ebd0e29c7af4145f857f05281eb9e8"
+	VoteVerifierVerificationKeyURL  = "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/voteverifier/voteverifier.vk"
+	VoteVerifierVerificationKeyHash = "a3a3874b6a1d4c568f6ee0d221e3213bf408f4e66d67e3f1eaf3c73f02994309"
+	// CircuitArtifacts constants for circuits/aggregator package
+	AggregatorProvingKeyURL       = "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/aggregator/aggregator.pk"
+	AggregatorProvingKeyHash      = "aecef25b7f5cd6c28df19d5398a7c9d6922149fdc60d7ebfee549eb42d84abe9"
+	AggregatorVerificationKeyURL  = "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/aggregator/aggregator.vk"
+	AggregatorVerificationKeyHash = "c748f9e234d70c0123f116a5a88b81ad1bcf782a9d9d0d50d2caa196aac2c0fb"
+	DummyProvingKeyURL            = "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/aggregator/dummy.pk"
+	DummyProvingKeyHash           = "fa587e9f24473de364d8950c70be11f6c33118b0be12df4ee100eed0dabecff2"
+)
