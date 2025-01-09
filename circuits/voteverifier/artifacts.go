455b665427bd79df790e9809123f3848a0cfbbdf
Lucas Menendez <hi@lucasmenendez.me>
2025-01-09 11:31:42 +0100

    including artifacts hashes

--- circuits/voteverifier/artifacts.go
+++ circuits/voteverifier/artifacts.go
@@ -1,8 +1,17 @@
 package voteverifier
 
-import "github.com/vocdoni/vocdoni-z-sandbox/circuits"
+import (
+	"github.com/vocdoni/vocdoni-z-sandbox/circuits"
+	"github.com/vocdoni/vocdoni-z-sandbox/types"
+)
 
 var Artifacts = circuits.NewCircuitArtifacts(
-	&circuits.Artifact{RemoteURL: "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/voteverifier/voteverifier.pk"},
-	&circuits.Artifact{RemoteURL: "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/voteverifier/voteverifier.vk"},
+	&circuits.Artifact{
+		RemoteURL: "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/voteverifier/voteverifier.pk",
+		Hash:      types.HexStringToHexBytes("4bcb2de78562f400a3f96e5adcdcc00d32ebd0e29c7af4145f857f05281eb9e8"),
+	},
+	&circuits.Artifact{
+		RemoteURL: "https://media.githubusercontent.com/media/vocdoni/vocdoni-circuits-artifacts/main/voteverifier/voteverifier.vk",
+		Hash:      types.HexStringToHexBytes("a3a3874b6a1d4c568f6ee0d221e3213bf408f4e66d67e3f1eaf3c73f02994309"),
+	},
 )
