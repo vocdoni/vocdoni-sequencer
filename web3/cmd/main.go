9c5fb7941e729d92e380876c355c43903c104170
p4u <pau@dabax.net>
2025-01-20 12:10:20 +0100

    add initial web3 packet with minimal/test implementation
    Signed-off-by: p4u <pau@dabax.net>

---
+++ web3/cmd/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/vocdoni/vocdoni-z-sandbox/crypto/ecc/curves"
+	"github.com/vocdoni/vocdoni-z-sandbox/crypto/elgamal"
+	"github.com/vocdoni/vocdoni-z-sandbox/log"
+	"github.com/vocdoni/vocdoni-z-sandbox/types"
+	"github.com/vocdoni/vocdoni-z-sandbox/util"
+	"github.com/vocdoni/vocdoni-z-sandbox/web3"
+)
+
+var rpcs = []string{
+	"wss://sepolia.drpc.org",
+	"https://sepolia.gateway.tenderly.co",
+	"https://rpc.ankr.com/eth_sepolia",
+	"https://eth-sepolia.public.blastapi.io",
+	"https://1rpc.io/sepolia",
+	"https://eth-sepolia-public.unifra.io",
+	"https://rpc.sepolia.ethpandaops.io",
+	"https://rpc-sepolia.rockx.com",
+	"wss://sepolia.gateway.tenderly.co",
+}
+
+func main() {
+	privKey := flag.String("privkey", "", "private key to use for the Ethereum account")
+	flag.Parse()
+	log.Init("debug", "stdout", nil)
+	contracts, err := web3.NewContracts(&web3.Addresses{
+		OrganizationRegistry: common.HexToAddress("0x3d0b39c0239329955b9F0E8791dF9Aa84133c861"),
+		ProcessRegistry:      common.HexToAddress("0xd512481d0Fa6d975f9B186a9f6e59ea8E12D2C2b"),
+	}, rpcs[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Infow("contracts initialized", "chainId", contracts.ChainID)
+
+	for i := 1; i < len(rpcs); i++ {
+		if err := contracts.AddWeb3Endpoint(rpcs[i]); err != nil {
+			log.Warnw("failed to add endpoint", "rpc", rpcs[i], "err", err)
+		}
+	}
+
+	if err := contracts.SetAccountPrivateKey(*privKey); err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	newProcChan, err := contracts.MonitorProcessCreationByPolling(ctx, time.Second*5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		log.Info("monitoring new processes")
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case proc := <-newProcChan:
+				log.Infow("new process found", "process", proc.ID)
+			}
+		}
+	}()
+
+	newOrgChan, err := contracts.MonitorOrganizationCreatedByPolling(ctx, time.Second*5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		log.Info("monitoring new organizations")
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case org := <-newOrgChan:
+				log.Infow("new organization found", "organization", org.String())
+			}
+		}
+	}()
+
+	time.Sleep(20 * time.Second)
+
+	orgAddr := common.HexToAddress(util.RandomHex(20))
+	if _, err := contracts.CreateOrganization(orgAddr, &types.OrganizationInfo{
+		Name:        fmt.Sprintf("Vocdoni test %x", orgAddr[:4]),
+		MetadataURI: "https://vocdoni.io",
+	}); err != nil {
+		log.Errorw(err, "failed to create organization")
+		return
+	}
+
+	curve := curves.New(curves.CurveTypeBN254)
+	pubKey, _, err := elgamal.GenerateKey(curve)
+	if err != nil {
+		log.Errorw(err, "failed to generate key")
+		return
+	}
+	x, y := pubKey.Point()
+
+	_, _, err = contracts.CreateProcess(&types.Process{
+		Status:         0,
+		OrganizationId: contracts.AccountAddress(),
+		EncryptionKey: &types.EncryptionKey{
+			X: x,
+			Y: y,
+		},
+		StateRoot:   util.RandomBytes(32),
+		StartTime:   time.Now().Add(5 * time.Minute),
+		Duration:    time.Hour,
+		MetadataURI: "https://example.com/metadata",
+		BallotMode: &types.BallotMode{
+			MaxCount:        2,
+			MaxValue:        *new(types.BigInt).SetUint64(100),
+			MinValue:        *new(types.BigInt).SetUint64(0),
+			MaxTotalCost:    *new(types.BigInt).SetUint64(0),
+			MinTotalCost:    *new(types.BigInt).SetUint64(0),
+			ForceUniqueness: false,
+			CostFromWeight:  false,
+		},
+		Census: &types.Census{
+			CensusRoot:   util.RandomBytes(32),
+			MaxVotes:     new(big.Int).SetUint64(100),
+			CensusURI:    "https://example.com/census",
+			CensusOrigin: 0,
+		},
+	})
+	if err != nil {
+		log.Errorw(err, "failed to create process")
+		return
+	}
+	select {}
+}
---
+++ web3/organization.go
@@ -0,0 +1,83 @@
+package web3
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/vocdoni/vocdoni-z-sandbox/log"
+	"github.com/vocdoni/vocdoni-z-sandbox/types"
+)
+
+// CreateOrganization creates a new organization in the OrganizationRegistry contract.
+func (c *Contracts) CreateOrganization(address common.Address, orgInfo *types.OrganizationInfo) (*common.Hash, error) {
+	txOpts, err := c.authTransactOpts()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create transact options: %w", err)
+	}
+	tx, err := c.organizations.CreateOrganization(txOpts, address, orgInfo.Name, orgInfo.MetadataURI, []common.Address{c.address})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create organization: %w", err)
+	}
+	hash := tx.Hash()
+	return &hash, nil
+}
+
+// Organization returns the organization with the given address from the OrganizationRegistry contract.
+func (c *Contracts) Organization(address common.Address) (*types.OrganizationInfo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), web3QueryTimeout)
+	nonce, name, uri, err := c.organizations.GetOrganization(&bind.CallOpts{Context: ctx}, address)
+	cancel()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get organization: %w", err)
+	}
+	return &types.OrganizationInfo{
+		Name:         name,
+		MetadataURI:  uri,
+		ProcessCount: nonce,
+	}, nil
+}
+
+// MonitorOrganizationCreatedByPolling monitors the creation of organizations by polling the logs of the blockchain.
+func (c *Contracts) MonitorOrganizationCreatedByPolling(ctx context.Context, interval time.Duration) (<-chan *types.OrganizationInfo, error) {
+	ch := make(chan *types.OrganizationInfo)
+	go func() {
+		defer close(ch)
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				log.Warnw("exiting monitor organizations creation")
+				return
+			case <-ticker.C:
+				ctxQuery, cancel := context.WithTimeout(ctx, web3QueryTimeout)
+				iter, err := c.organizations.FilterOrganizationCreated(&bind.FilterOpts{Start: c.lastWatchOrgBlock, Context: ctxQuery}, nil, nil)
+				cancel()
+				if err != nil || iter == nil {
+					log.Warnw("failed to filter organization created, retrying", "err", err)
+					continue
+				}
+				for iter.Next() {
+					id := fmt.Sprintf("%x", iter.Event.Id)
+					if _, exists := c.knownOrganizations[id]; exists {
+						continue
+					}
+					c.knownOrganizations[id] = struct{}{}
+					org, err := c.Organization(iter.Event.Id)
+					if err != nil {
+						log.Errorw(err, "failed to get organization while monitoring")
+						continue
+					}
+					org.ID = iter.Event.Id
+					c.lastWatchOrgBlock = iter.Event.Raw.BlockNumber
+					ch <- org
+				}
+			}
+		}
+	}()
+	return ch, nil
+
+}
