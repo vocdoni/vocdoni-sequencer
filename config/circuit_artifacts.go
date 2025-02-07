858a9fe60e9f9257534166045eddee126f5db8eb
Lucas Menendez <hi@lucasmenendez.me>
2025-02-07 14:40:24 +0100

    wrong uri for artifacts

--- config/circuit_artifacts.go
+++ config/circuit_artifacts.go
@@ -2,31 +2,31 @@ package config
 
 const (
 	// BallotProof constants for github.com/vocdoni/z-ircuits
-	BallotProoCircuitURL           = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/ballot_proof.wasm"
+	BallotProoCircuitURL           = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/ballot_proof.wasm"
 	BallotProofCircuitHash         = "c9aa004cff03cce4a9b347b8d09f8f771ad608180dc0249354c0079243abcb50"
-	BallotProofProvingKeyURL       = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/ballot_proof_pkey.zkey"
+	BallotProofProvingKeyURL       = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/ballot_proof_pkey.zkey"
 	BallotProofProvingKeyHash      = "a9a83a8a446e4d84c9fd5342c0ec9f00d86d3d3884cf5dcae731c222b358ab1f"
-	BallotProofVerificationKeyURL  = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/ballot_proof_vkey.json"
+	BallotProofVerificationKeyURL  = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/ballot_proof_vkey.json"
 	BallotProofVerificationKeyHash = "3e7a0b24250c6fea97c0950445cf104091c00bfd32796e8e8753955ab015429a"
 	// CircuitArtifacts constants for circuits/voteverifier package
-	VoteVerifierCircuitURL          = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/voteverifier.ccs"
+	VoteVerifierCircuitURL          = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/voteverifier.ccs"
 	VoteVerifierCircuitHash         = "e6e5e27a311c04ffe7894494e614f1cf28067df357b1d55b38855b8351e01f79"
-	VoteVerifierProvingKeyURL       = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/voteverifier.pk"
+	VoteVerifierProvingKeyURL       = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/voteverifier.pk"
 	VoteVerifierProvingKeyHash      = "4bf35e1f0ed36dc2bffa5f66bae476f16e7d03f07a955a5e24eea0e1953d4cec"
-	VoteVerifierVerificationKeyURL  = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/voteverifier.vk"
+	VoteVerifierVerificationKeyURL  = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/voteverifier.vk"
 	VoteVerifierVerificationKeyHash = "7f7284e2427269569bc1678ceaa681407081cd558265e630e9473d8afee9886f"
 	// CircuitArtifacts constants for circuits/aggregator package
-	AgregatorCircuitURL           = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/aggregator.ccs"
+	AgregatorCircuitURL           = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/aggregator.ccs"
 	AggregatorCircuitHash         = "15ae36338a1d6dec9f52c93cb906bb70f170df1307b537a6db1661b8948a7203"
-	AggregatorProvingKeyURL       = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/aggregator.pk"
+	AggregatorProvingKeyURL       = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/aggregator.pk"
 	AggregatorProvingKeyHash      = "fb4403686551be6e57166ca88e24950a2babeff59d4b7b901d939ae71f7477b0"
-	AggregatorVerificationKeyURL  = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/aggregator.vk"
+	AggregatorVerificationKeyURL  = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/aggregator.vk"
 	AggregatorVerificationKeyHash = "5c761cb20a9e125fbc73ebaea3372857948034085e4c6188187dd84555054e3a"
 	// CircuitArtifacts constants for circuits/dummy package
-	DummyCircuitURL          = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/dummy.ccs"
+	DummyCircuitURL          = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/dummy.ccs"
 	DummyCircuitHash         = "4bbe70e717f08d5e337306aea4c1c1c926a5cf1a24ab223d318023eb4520abb8"
-	DummyProvingKeyURL       = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/dummy.pk"
+	DummyProvingKeyURL       = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/dummy.pk"
 	DummyProvingKeyHash      = "89915d5607c701dced3dfb6a7a3a26a3d343a150a47ac0beb049063bf2701db9"
-	DummyVerificationKeyURL  = "https://circuits.ams3.cdn.digitaloceanspaces.com/circuits/dev/dummy.vk"
+	DummyVerificationKeyURL  = "https://circuits.ams3.cdn.digitaloceanspaces.com/dev/dummy.vk"
 	DummyVerificationKeyHash = "1dd3badb7d4f1dc2ac4620dbdb55da7c066f2dbd7881635cf40544c4f774be77"
 )
