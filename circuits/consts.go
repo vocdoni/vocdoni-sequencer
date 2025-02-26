b53920d1c626fadf6b60eadc5144df2777a91f75
Guido Iribarren <git@guidoi.com.ar>
2025-02-26 11:09:02 +0100

    (debug) reduce VotesPerBatch=3

--- circuits/consts.go
+++ circuits/consts.go
@@ -3,7 +3,7 @@ package circuits
 // used across different circuits
 const (
 	FieldsPerBallot      = 8
-	VotesPerBatch        = 10
+	VotesPerBatch        = 3
 	CensusProofMaxLevels = 160
 	StateProofMaxLevels  = 160
 )
