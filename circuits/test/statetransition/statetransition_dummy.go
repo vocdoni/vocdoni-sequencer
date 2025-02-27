448992f23292bdc82a77c1b933ebb82c5003c7a8
Guido Iribarren <git@guidoi.com.ar>
2025-02-27 10:27:30 +0100

    circuits: drop unused dummy circuit

--- circuits/test/statetransition/statetransition_dummy.go
+++ circuits/test/statetransition/statetransition_dummy.go
@@ -3,15 +3,18 @@ package statetransitiontest
 import (
 	"errors"
 	"fmt"
+	"math/big"
 
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/backend/witness"
 	"github.com/consensys/gnark/constraint"
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_bw6761"
 	"github.com/consensys/gnark/std/math/emulated"
-	"github.com/consensys/gnark/std/recursion/groth16"
+	stdgroth16 "github.com/consensys/gnark/std/recursion/groth16"
 	"github.com/vocdoni/vocdoni-z-sandbox/circuits"
-	"github.com/vocdoni/vocdoni-z-sandbox/circuits/dummy"
 )
 
 // DummyAggCircuit is dummy aggregator circuit
@@ -68,17 +71,17 @@ func DummyAggAssignment(inputHash, validVotes frontend.Variable) *DummyAggCircui
 
 // DummyAggProofPlaceholder returns a placeholder for a dummy aggregator proof
 func DummyAggProofPlaceholder() (
-	*groth16.Proof[sw_bw6761.G1Affine, sw_bw6761.G2Affine],
-	*groth16.VerifyingKey[sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl],
+	*stdgroth16.Proof[sw_bw6761.G1Affine, sw_bw6761.G2Affine],
+	*stdgroth16.VerifyingKey[sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl],
 ) {
-	ccs, _, vk, err := dummy.CompileAndSetup(DummyAggPlaceholderWithConstraints(0), circuits.AggregatorCurve.ScalarField())
+	ccs, _, vk, err := CompileAndSetup(DummyAggPlaceholderWithConstraints(0), circuits.AggregatorCurve.ScalarField())
 	if err != nil {
 		panic(err)
 	}
 	// parse dummy proof and witness
-	dummyProof := groth16.PlaceholderProof[sw_bw6761.G1Affine, sw_bw6761.G2Affine](ccs)
+	dummyProof := stdgroth16.PlaceholderProof[sw_bw6761.G1Affine, sw_bw6761.G2Affine](ccs)
 	// set fixed dummy vk in the placeholders
-	dummyVK, err := groth16.ValueOfVerifyingKeyFixed[sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl](vk)
+	dummyVK, err := stdgroth16.ValueOfVerifyingKeyFixed[sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl](vk)
 	if err != nil {
 		panic(err)
 	}
@@ -87,24 +90,59 @@ func DummyAggProofPlaceholder() (
 
 // DummyAggProof returns a dummy aggregator proof
 func DummyAggProof(inputsHash, validVotes frontend.Variable) (
-	*groth16.Proof[sw_bw6761.G1Affine, sw_bw6761.G2Affine],
-	*groth16.VerifyingKey[sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl], error,
+	*stdgroth16.Proof[sw_bw6761.G1Affine, sw_bw6761.G2Affine],
+	*stdgroth16.VerifyingKey[sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl], error,
 ) {
-	_, _, proof, vk, err := dummy.Prove(
+	_, _, proof, vk, err := Prove(
 		DummyAggPlaceholderWithConstraints(0), DummyAggAssignment(inputsHash, validVotes),
 		circuits.StateTransitionCurve.ScalarField(), circuits.AggregatorCurve.ScalarField())
 	if err != nil {
 		return nil, nil, err
 	}
 	// parse dummy proof and witness
-	dummyProof, err := groth16.ValueOfProof[sw_bw6761.G1Affine, sw_bw6761.G2Affine](proof)
+	dummyProof, err := stdgroth16.ValueOfProof[sw_bw6761.G1Affine, sw_bw6761.G2Affine](proof)
 	if err != nil {
 		return nil, nil, fmt.Errorf("dummy proof value error: %w", err)
 	}
 	// set fixed dummy vk in the placeholders
-	dummyVK, err := groth16.ValueOfVerifyingKeyFixed[sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl](vk)
+	dummyVK, err := stdgroth16.ValueOfVerifyingKeyFixed[sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl](vk)
 	if err != nil {
 		return nil, nil, fmt.Errorf("dummy vk value error: %w", err)
 	}
 	return &dummyProof, &dummyVK, nil
+}
+
+func Prove(placeholder, assignment frontend.Circuit, outer *big.Int, field *big.Int) (constraint.ConstraintSystem, witness.Witness, groth16.Proof, groth16.VerifyingKey, error) {
+	ccs, pk, vk, err := CompileAndSetup(placeholder, field)
+	if err != nil {
+		return nil, nil, nil, nil, fmt.Errorf("init error: %w", err)
+	}
+	fullWitness, err := frontend.NewWitness(assignment, field)
+	if err != nil {
+		return nil, nil, nil, nil, fmt.Errorf("full witness error: %w", err)
+	}
+	proof, err := groth16.Prove(ccs, pk, fullWitness, stdgroth16.GetNativeProverOptions(outer, field))
+	if err != nil {
+		return nil, nil, nil, nil, fmt.Errorf("proof error: %w", err)
+	}
+	publicWitness, err := fullWitness.Public()
+	if err != nil {
+		return nil, nil, nil, nil, fmt.Errorf("pub witness error: %w", err)
+	}
+	if err = groth16.Verify(proof, vk, publicWitness, stdgroth16.GetNativeVerifierOptions(outer, field)); err != nil {
+		return nil, nil, nil, nil, fmt.Errorf("verify error: %w", err)
+	}
+	return ccs, publicWitness, proof, vk, nil
+}
+
+func CompileAndSetup(placeholder frontend.Circuit, field *big.Int) (constraint.ConstraintSystem, groth16.ProvingKey, groth16.VerifyingKey, error) {
+	ccs, err := frontend.Compile(field, r1cs.NewBuilder, placeholder)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("compile error: %w", err)
+	}
+	pk, vk, err := groth16.Setup(ccs)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("setup error: %w", err)
+	}
+	return ccs, pk, vk, nil
 }
